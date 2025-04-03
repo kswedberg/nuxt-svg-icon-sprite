2830fdfdbbdc9c84be35ad45d809f22315051e9f
Karl Swedberg <kswedberg@gmail.com>
2025-04-03 13:14:04 -0400

    Add "nested route" page in playground for testing

--- playground/pages/spa/index.vue
+++ playground/pages/spa/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <h1 class="font-bold text-5xl mb-8">Test Icons</h1>
-
+    <h1 class="font-bold text-5xl mb-2">Test Icons</h1>
+    <div class="mb-2">
+      <nuxt-link class="text-blue-700" to="/spa/nested">nested route</nuxt-link>
+    </div>
     <AllSymbols />
 
     <MultipleSymbols />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useHead } from '#imports'
+useHead({
+  title: 'SPA - SVG Icon Sprite Playground',
+})
+</script>
---
+++ playground/pages/spa/nested.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="font-bold text-5xl mb-8">Test Icons</h1>
+
+    <AllSymbols />
+
+    <MultipleSymbols />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useHead } from '#imports'
+useHead({
+  title: 'Nested Route - SVG Icon Sprite Playground',
+})
+</script>
